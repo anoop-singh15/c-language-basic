41b90d9581c7a6369ad34fd15d2d3a87bf5b5f7f
WELCOME NAMITA <cozytuba1533@gmail.com>
2022-11-02 19:48:59 +0530

    c-second commit

---
+++ exercise11Palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int palindrome(int num)
+{
+    int digit = 0;
+    while (num != 0)
+    {
+        int a;
+        digit = digit * 10 + num % 10;
+        num = num / 10;
+    }
+    return digit;
+}
+int main()
+{
+    int number, revnumber;
+    printf("Enter a number\n");
+    scanf("%d", &number);
+    revnumber = palindrome(number);
+
+    if (number == revnumber)
+    {
+        printf("palindrome number\n");
+        printf("%d\n",revnumber);
+    }
+    else
+    {
+        printf("Not a palindrome number\n");
+         printf("%d\n",revnumber);
+    }
+
+    return 0;
+}
